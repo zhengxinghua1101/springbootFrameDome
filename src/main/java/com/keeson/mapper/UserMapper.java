aac8e7258d75a53b86af179f9c90dbe6f80af750
Jeff.zheng <Jeff.zheng@keeson.com>
2022-04-09 10:29:11 +0800

    多环境配置 + 多数据源配置

--- src/main/java/com/keeson/mapper/UserMapper.java
+++ src/main/java/com/keeson/mapper/UserMapper.java
@@ -1,9 +1,11 @@
 package com.keeson.mapper;
 
+import com.baomidou.dynamic.datasource.annotation.DS;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.keeson.vo.User;
 import org.springframework.stereotype.Repository;
 
+@DS("master")
 @Repository //声明持久层操作的bean
 public interface UserMapper  extends BaseMapper<User> {
 
