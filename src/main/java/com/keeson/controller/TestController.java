3a1d75caefd9a243de9160280667a0816459da1c
Jeff.zheng <Jeff.zheng@keeson.com>
2022-06-27 16:47:16 +0800

    test

--- src/main/java/com/keeson/controller/TestController.java
+++ src/main/java/com/keeson/controller/TestController.java
@@ -4,12 +4,13 @@ import cn.hutool.core.date.DateUtil;
 import com.keeson.common.ResultHandler;
 import com.keeson.service.UserService;
 import com.keeson.service.impl.UserServiceImpl;
+import com.keeson.vo.ExcelPoi;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @Api(tags = "测试接口")
@@ -30,6 +31,32 @@ public class TestController {
         System.out.println("666==" + userServiceimpl.getById(66));
         return ResultHandler.success("1212121", DateUtil.now());
     }
+
+
+    @PostMapping("/api/v2/detection/productionSave")
+    ResultHandler productionSave(@RequestParam Map<String ,Object> map)
+    {
+        System.out.println("开始"+map.toString());
+        return ResultHandler.success(map.toString());
+    }
+
+//    @PostMapping("/api/v2/detection/productionSave")
+//    ResultHandler productionSave1(@RequestBody Map<String ,Object> map)
+//    {
+//        System.out.println("开始1"+map.toString());
+//        return ResultHandler.success(map.toString());
+//    }
+
+
+    @GetMapping("/my_test")
+    ResultHandler my_test(ExcelPoi excelPoi)
+    {
+        System.out.println(excelPoi);
+        System.out.println("开始");
+        return ResultHandler.success();
+    }
+
+
 
 
     @PostMapping("/sms")
